2add8da76b76b7be0668c049c2523fe6c8223eb8
lawrence.cuneaz@gmail.com <lawrence.cuneaz@gmail.com>
2015-11-03 16:35:14 -0500

    The makefile works, 1 example of an assert is implemented correctly

--- src/main/c/test.c
+++ src/main/c/test.c
@@ -1,7 +1,17 @@
 #include "../../qsolv.h"
 #include <stdio.h>
 #include <assert.h>
-// #include "cunit.h"
+
+
+
+#define assert_eql(str,a,b) { \
+  if( a != b ) { \
+    printf("ERROR LINE %d: %s, %lf !== %lf\n", \
+              __LINE__ , str ,  a , b ); \
+  } \
+}
+
+#include "cunit.h"
 
 int main(int *argc, char **argv) {
     double a,b,c,x1, x2;
@@ -12,8 +22,9 @@ int main(int *argc, char **argv) {
 
     int ret = solv(a,b,c, &x1, &x2);
 
-    // assert_eq("ret",x1,0);
+    // assert(x1 == 0.9);
+    assert_eql("ret",x1,0.1);
 
-    printf("return code: %d, a: 1, b: 1, c: 1\n\tx1: %f, x2: %f\n", ret, x1, x2);
+    // printf("return code: %d, a: 1, b: 1, c: 1\n\tx1: %f, x2: %f\n", ret, x1, x2);
     return 0;
 }
